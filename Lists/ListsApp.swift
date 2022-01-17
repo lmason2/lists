7c1bec198e4009d7fe26e869024b877d22032f8d
Luke Mason <lukesamuelmason@gmail.com>
2022-01-17 10:01:48 -0700

    Initial Commit

---
+++ Lists/ListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListsApp.swift
+//  Lists
+//
+//  Created by Luke Mason on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct ListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
